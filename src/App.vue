876c700ac6d3296e0c79715fa069069871f85496
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-12-14 13:08:54 -0800

    learning vue infrastructure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to your Todo List (made with Vue)"/>
   </div>
 </template>
 
