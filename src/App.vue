0c0097eb0e87d818950ecc656b7879fa49307a65
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-12-14 13:41:41 -0800

    we can now see to do items

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,40 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to your Todo List (made with Vue)"/>
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Todos
+  },
+  data(){
+  return {
+    todos: [
+      {
+      id:1,
+      title: 'Workout',
+      completed: false
+      },
+      {
+      id:1,
+      title: 'Eat ',
+      completed: false
+      },
+      {
+      id:1,
+      title: 'Take shower',
+      completed: false
+      }
+    ]
+  }}
 }
 </script>
 
---
+++ src/components/Todos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>My To Do List</h2>
+    <ul>
+      <li v-bind:key="todo.id" v-for="todo in todos">
+        <Todo v-bind:todo="todo" />
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+
+<script>
+import Todo from './Todo';
+
+export default {
+  name:'Todos',
+  components: {
+    Todo 
+    },
+    props: [
+      "todos"
+      ]
+    }
+
+</script>
+
+<style scoped></style>
